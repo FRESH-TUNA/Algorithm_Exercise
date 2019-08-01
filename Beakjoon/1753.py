e667310b0084cb90119d721f3e203a5b7cfce2b0
DONGWON KIM <lunacircle4@gmail.com>
2019-08-01 13:58:49 +0900

    1753 dijkstra

--- Beakjoon/1753.py
+++ Beakjoon/1753.py
@@ -1,63 +1,55 @@
-from heapq import heappush, heappop, heapify
+import queue
 import sys
-class Beakjoon_1753_solver:
-    array = []
-    min = []
-    num_of_vertax = None
+
+class Beakjoon_1753_solver():
+    num_of_vertex = None
     num_of_edges = None
     start = None
+    adj = [[]]
+    result = []
 
     @classmethod
     def init(clr):
-        clr.num_of_vertax, clr.num_of_edges = list(map(int, input().split()))
-        clr.visited = (clr.num_of_vertax + 1) * [False]
-        clr.start = int(input())
-        clr.minHeap = [[sys.maxsize, x] for x in range(clr.num_of_vertax + 1)] 
-        clr.minHeap[clr.start][1] = 0
-        clr.min = [sys.maxsize] * (clr.num_of_vertax + 1)
-        clr.min[clr.start] = 0
-        
+        clr.num_of_vertex, clr.num_of_edges = map(int, sys.stdin.readline().split())
+        clr.start = int(sys.stdin.readline())
+        clr.adj = [[] for i in range(clr.num_of_vertex + 1)]
+        clr.result = [sys.maxsize] * (clr.num_of_vertex + 1) 
+
+        while clr.num_of_edges:
+            clr.num_of_edges -= 1
+            start, end, weight = map(int, sys.stdin.readline().split())
+            clr.adj[start].append((end, weight))
+
+
     @classmethod
-    def service(clr):
-        visitedCount = 1
-        start = clr.start
+    def dijkstra(clr):
+        pq = queue.PriorityQueue()
+        pq.put((0, clr.start))
+        clr.result[clr.start] = 0
 
-        while clr.minHeap:
-            heapMin = heappop(clr.minHeap)
-            if heapMin[0] <= clr.min[heapMin[1]]:
-                for x in range(vertex):
-                    if clr.min[heapMin[1]] > clr.min[x] + array[x][heapMin[1]]:
-                        clr.min[heapMin[1]] = clr.min[x] + array[x][heapMin[1]]
+        while not pq.empty():
+            distance, destination = pq.get()
 
+            if clr.result[destination] < distance:
+                continue
+            
+            for i in clr.adj[destination]:
+                cost = distance + i[1]
+                if clr.result[i[0]] > cost:
+                    clr.result[i[0]] = cost
+                    pq.put((cost, i[0]))
 
-
-        while visitedCount < len(clr.visited):
-            for edge in clr.array:
-                if edge[1] == start:
-                    if clr.min[edge[1]] + edge[0] < clr.min[edge[2]]:
-                        clr.min[edge[2]] = clr.min[edge[1]] + edge[0]
-                        if clr.visited[edge[2]] is False:
-                            heappush(clr.minHeap, [clr.min[edge[1]] + edge[0], edge[2]])
-
-            clr.visited[start] = True
-            visitedCount += 1
-            tempNext = None
-
-            if len(clr.minHeap) is 0:
-                break
+    @classmethod
+    def print(clr):
+        for i in range (1,clr.num_of_vertex + 1):
+            if clr.result[i] is sys.maxsize:
+                print("INF")
             else:
-                tempNext = heappop(clr.minHeap)
-                start = tempNext[1]
-
-        for x in range(1, len(clr.min)):
-            if clr.min[x] is sys.maxsize:
-                print('INF')
-            else:
-                print(clr.min[x])
-
-
+                print(clr.result[i])
 
 if __name__ == "__main__":
     Beakjoon_1753_solver.init()
-    Beakjoon_1753_solver.service()
+    Beakjoon_1753_solver.dijkstra()
+    Beakjoon_1753_solver.print()
+    
     
