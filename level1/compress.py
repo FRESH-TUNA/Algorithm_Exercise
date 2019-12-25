540d5cb8bd0d20a850e8f559c664bd6734ac6f9b
DONGWON KIM <lunacircle4@likelion.org>
2019-12-25 23:23:04 +0900

    기능: chunk의 길이를 조절할수 있도록 프로토타입 구현

--- level1/compress.py
+++ level1/compress.py
@@ -1,24 +1,57 @@
-def append_chunk(amount, source, chunk):
+def append_chunk(source, amount, chunk):
     if amount == 1:
         return source + chunk
     else:
         return source + str(amount) + chunk
 
-def compress(data, chunk, amount, result):
-    for x in data:
-        if x == chunk:
-            amount = amount + 1
-        else:
-            result = append_chunk(amount, result, chunk)
-            chunk = x
-            amount = 1
-    return append_chunk(amount, result, chunk)
+def get_chunk(data, chunk_len):
+    return data[:chunk_len]
+
+def remove_chunk(data, chunk_len):
+    return data[chunk_len:]
+
+def check_not_finish_parsing_data(data, chunk_len):
+    return len(data) >= chunk_len
+
+def get_max_chunk_len(length):
+    return length // 2
+
+def add_remain_data(data, remain_data):
+  return data + remain_data
+
+def compress(data, chunk_len):
+  compared_from = get_chunk(data, chunk_len)
+  chunk_count = 0
+  compressed_data = ''
+  while check_not_finish_parsing_data(data, chunk_len):
+    compared_to = get_chunk(data, chunk_len)
+    data = remove_chunk(data, chunk_len)
+    if compared_from == compared_to:
+      chunk_count = chunk_count + 1
+    else:
+      compressed_data = append_chunk(
+        compressed_data, chunk_count, compared_from
+      )
+      chunk_count = 1
+      compared_from = compared_to
+  
+  compressed_data = append_chunk(compressed_data, chunk_count, compared_from)
+  print(data)
+  return add_remain_data(compressed_data, data)
+
 
 def calculate_min_length_of_compress(data):
-    chunk = data[0]
-    amount = 0
-    result = ''
-    return compress(data, chunk, amount, result)
+    max_chunk_len = get_max_chunk_len(len(data))
+    chunk_len = 1
+    min_length = len(data)
+
+    while chunk_len < max_chunk_len:
+        min_length = min(
+            min_length,
+            len(compress(data, chunk_len))
+        )
+    return min_length
+
 
 if __name__ == "__main__":
     s = input()
