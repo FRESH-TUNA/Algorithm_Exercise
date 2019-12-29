5b899c17ab05aa22e26372b3c59bf91634be2842
DONGWON KIM <lunacircle4@likelion.org>
2019-12-29 22:33:34 +0900

    기능: compress 문제, OOP로 구현 완료

--- level1/compress_oop.py
+++ level1/compress_oop.py
@@ -1,55 +1,60 @@
 class Compressor:
-  def __init__(self, data):
+  def __init__(self, data, chunk_len):
     self.data = data
+    self.chunk_len = chunk_len
+    self.compressed_data = ''
 
-  def append_chunk(self, source, amount, chunk):
+  def append_chunk(self, amount, chunk):
     if amount == 1:
-      return source + chunk
+      self.compressed_data += chunk
     else:
-      return source + str(amount) + chunk
+      self.compressed_data += str(amount) + chunk
 
-  def get_chunk(self, remain_data, chunk_len):
-    return remain_data[:chunk_len]
+  def get_chunk(self):
+    return self.data[:self.chunk_len]
 
-  def remove_chunk(self, remain_data, chunk_len):
-    return remain_data[chunk_len:]
+  def pop_chunk(self):
+    poped = self.data[:self.chunk_len]
+    self.data = self.data[self.chunk_len:]
+    return poped
 
-  def check_not_finish_parsing_data(self, remain_data, chunk_len):
-    return len(remain_data) >= chunk_len
+  def check_not_finish_parsing_data(self):
+    return len(self.data) >= self.chunk_len
 
-  def get_max_chunk_len(self, length):
-    return length // 2
+  def add_remain_data(self):
+    self.compressed_data += self.data
 
-  def add_remain_data(self, data, remain_data):
-    return data + remain_data
+  def post_processing(self, chunk_count, compared_from):
+    self.append_chunk(chunk_count, compared_from)
+    self.add_remain_data()
 
-  def _compress_initial_data(self, chunk_len):
-    compared_from = self.get_chunk(self.data, chunk_len)
+  def compress(self):
     chunk_count = 0
-    compressed_data = ''
-    remain_data = self.data
-    return [compared_from, chunk_count, compressed_data, remain_data]
+    compared_from = self.get_chunk()
+    compared_to = ''
 
-  def compress(self, chunk_len):
-    compared_from, chunk_count, compressed_data, remain_data = self._compress_initial_data(chunk_len)
-
-    while self.check_not_finish_parsing_data(remain_data, chunk_len):
-      compared_to = self.get_chunk(remain_data, chunk_len)
-      remain_data = self.remove_chunk(remain_data, chunk_len)
+    while self.check_not_finish_parsing_data():
+      compared_to = self.pop_chunk()
 
       if compared_from == compared_to:
         chunk_count = chunk_count + 1
       else:
-        compressed_data = self.append_chunk(
-          compressed_data, chunk_count, compared_from
-        )
+        self.append_chunk(chunk_count, compared_from)
         chunk_count = 1
         compared_from = compared_to
 
-        compressed_data = self.append_chunk(
-          compressed_data, chunk_count, compared_from
-        )
-        return self.add_remain_data(compressed_data, remain_data)
+    self.post_processing(chunk_count, compared_from)
+    return self.compressed_data
+
+
+
+
+class Calculator:
+  def __init__(self, data):
+    self.data = data
+
+  def get_max_chunk_len(self, length):
+    return length // 2
 
   def _calculate_min_length_of_compress_initial_data(self):
     max_chunk_len = self.get_max_chunk_len(len(self.data))
@@ -61,16 +66,16 @@ class Compressor:
     max_chunk_len, chunk_len, min_length = self._calculate_min_length_of_compress_initial_data()
 
     while chunk_len <= max_chunk_len:
+      compressor = Compressor(self.data, chunk_len)
       min_length = min(
         min_length,
-        len(self.compress(chunk_len))
+        len(compressor.compress())
       )
       chunk_len = chunk_len + 1
     
     return min_length
 
-
 if __name__ == "__main__":
   s = input()
-  calculater = Compressor(s)
+  calculater = Calculator(s)
   print(calculater.calculate_min_length_of_compress())
