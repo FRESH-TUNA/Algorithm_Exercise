e23be09e4fb1bfa96f6209f15143338cf91e23cd
DONGWON KIM <lunacircle4@likelion.org>
2020-01-12 23:36:39 +0900

    기능: level2/printer 문제 리펙토링

--- level2/printer.py
+++ level2/printer.py
@@ -1,28 +1,50 @@
 class Printer:
     def __init__(self, priorities):
         self.priorities = priorities
-        self.sequence = list(range(len(self.priorities)))
+        self.documents = list(range(len(self.priorities)))
 
-    def get_sequence(self, location):
+    def get_sequence(self, document):
         count = 1
+
         while len(self.priorities) != 0:
-            normal_seq = self.sequence_change()
-            if normal_seq == location:
+            normal_seq = self.print()
+            if normal_seq == document:
                 return count
-            elif normal_seq != -1:
+            else:
                 count = count + 1
+
         return count
 
-    def sequence_change(self):        
-        for index, value in enumerate(self.priorities):
-            if value > self.priorities[0]:
-                poped_value = self.priorities.pop(0)
-                self.priorities.append(poped_value)
-                poped_value = self.sequence.pop(0)
-                self.sequence.append(poped_value)
-                return -1
+    def print(self):
+        i = 1      
+
+        while not self.finish_iteration(i):
+            if self.has_high_priority(self.priorities[i]):
+                self.sequence_change()
+                i = 1
+            else:
+                i = i + 1
+        
         self.priorities.pop(0)
-        return self.sequence.pop(0)
+        return self.documents.pop(0)
+    
+    def has_high_priority(self, value):
+        return value > self.priorities[0]
+
+    def finish_iteration(self, i):
+        return i >= len(self.priorities)
+
+    def sequence_change(self):
+        self.priorities_pop_and_append()
+        self.documents_pop_and_append()
+
+    def priorities_pop_and_append(self):
+        poped_value = self.priorities.pop(0)
+        self.priorities.append(poped_value)
+
+    def documents_pop_and_append(self):
+        poped_value = self.documents.pop(0)
+        self.documents.append(poped_value)
 
 if __name__ == "__main__":
     printer = Printer([2, 1, 3, 2])
