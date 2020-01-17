1b6a3fdec46e8fdc2e0445d31193704605a743c4
DONGWON KIM <33149791+lunacircle4@users.noreply.github.com>
2020-01-18 00:49:58 +0900

    기능: cutter, O(n^2) 구현

--- level2/cutter.py
+++ level2/cutter.py
@@ -1,8 +1,36 @@
-def calculate_lazer_points(data):
-    lazer_points = []
-    left_bracket_stack = []
-    
-    for bracket in data:
-        if bracket is 
-        
-    return lazer_points
+class Cutter:
+    def __init__(self, data):
+        self.lasers = []
+        self.steel_bars = []
+        self.left_bracket_stack = []
+        self.data = data
+
+    def seperate(self):
+        for index, bracket in enumerate(self.data):
+            if bracket is '(':
+                self.left_bracket_stack.append(index)
+            elif self.is_laser(index):
+                self.lasers.append(self.left_bracket_stack.pop())
+            else:
+                self.steel_bars.append((self.left_bracket_stack.pop(), index))
+
+    def last_of_left_bracket_stack(self):
+        return self.left_bracket_stack[-1]
+
+    def is_laser(self, index):
+        return self.last_of_left_bracket_stack() + 1 == index
+
+    def cut(self):
+        count = len(self.steel_bars)
+        for steel_bar in self.steel_bars:
+            for laser in self.lasers:
+                if self.cuttable(laser, steel_bar):
+                    count = count + 1
+        return count
+
+    def cuttable(self, laser, steel_bar):
+        return laser >= steel_bar[0] and laser < steel_bar[1]
+
+    def service(self):
+        self.seperate()
+        return self.cut()
