a52ec7bbc2feaf67fd8ec8edcd48b019b290a517
DONGWON KIM <33149791+lunacircle4@users.noreply.github.com>
2020-02-20 00:17:10 +0900

    feat: level2/bignumbermake problem, 자료형 변경

--- level2/bigNumberMake.java
+++ level2/bigNumberMake.java
@@ -1,13 +1,13 @@
 class Solution {
   public String max_calculator(String number) {
-    int max_number = -1;
+    String max_number = number.substring(1, number.length());
     for(int i = 0; i < number.length(); i++) {
       String candidate = number.substring(0,i) + number.substring(i + 1, number.length());
-      if(Integer.parseInt(candidate) > max_number) {
-        max_number = Integer.parseInt(candidate);
+      if(candidate.compareTo(max_number) > 0) {
+        max_number = candidate;
       }
     }
-    return Integer.toString(max_number);
+    return max_number;
   }
   public String solution(String number, int k) {
     String max_number = number;
