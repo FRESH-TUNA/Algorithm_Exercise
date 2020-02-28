d940765f82436033d48f1e6e78520e6c05818262
DONGWON KIM <lunacircle4@likelion.org>
2020-02-29 00:41:58 +0900

    feat: level2/bigNumbermake oop apply prototype

--- level2/bigNumberMake.java
+++ level2/bigNumberMake.java
@@ -1,58 +1,55 @@
-// class Solution {
-//   public String max_calculator(String number) {
-//     String max_number = number.substring(1, number.length());
-//     for(int i = 0; i < number.length(); i++) {
-//       String candidate = number.substring(0,i) + number.substring(i + 1, number.length());
-//       if(candidate.compareTo(max_number) > 0) {
-//         max_number = candidate;
-//       }
-//     }
-//     return max_number;
-//   }
-//   public String solution(String number, int k) {
-//     String max_number = number;
-//     for(int i = 0; i < k; i++) {
-//       max_number = max_calculator(max_number);
-//     }
-//     return max_number;
-//   }
-// }
-
 import java.util.Stack;
 import java.util.Iterator;
 class BlgNumberMakeService {
-    public int stack_check(Stack<Character> stack, Character lastst_pushed, int k){
-    while(!stack.empty() && k > 0) {
-      if(stack.peek() < lastst_pushed) {
+  private String number;
+  private int countOfDelete;
+  private Stack<Character> stack;
+
+  public BlgNumberMakeService(String number, int countOfDelete) {
+    this.number = number;
+    this.countOfDelete = countOfDelete;
+    this.stack = new Stack<Character>();
+  }
+
+  public String call() {
+    this.preprocessStack();
+    this.bigNumberStackMake();
+    return this.getBigNumber();
+  }
+
+  private void preprocessStack() {
+    this.stack.push(number.charAt(0));
+  }
+
+  private void bigNumberMake() {
+    for(int i = 1; i < number.length(); i++) {
+      if(stack.peek() < number.charAt(i)) {
+          this.stack_pop(stack, number.charAt(i));
+      else 
+          stack.push(number.charAt(i));
+    }
+  }
+
+  private void getBigNumber() {
+    Iterator<Character> iterator = stack.iterator();
+    StringBuilder sb = new StringBuilder();
+
+    while(iterator.hasNext()) {
+        sb.append(iterator.next());
+    }
+
+    return sb.substring(0, sb.length() - k).toString();
+  }
+
+  private void stack_pop(Character lastst_pushed) {
+    while(!this.stack.empty() && this.number > 0) {
+      if(this.stack.peek() < lastst_pushed) {
         stack.pop();
-        k = k - 1;
+        this.number--;
       }
       else
         break;
     }
-    stack.push(lastst_pushed);
-    return k;
-   }
-    
-  public String solution(String number, int k) {
-    Stack<Character> stack = new Stack<Character>();
-    stack.push(number.charAt(0));
-        
-    for(int i = 1; i < number.length(); i++) {
-        if(stack.peek() < number.charAt(i)) {
-            k = stack_check(stack, number.charAt(i), k);
-        }
-        else {
-            stack.push(number.charAt(i));
-        }
-    }
-    StringBuilder sb = new StringBuilder();
-
-    Iterator<Character> iterator = stack.iterator();
-    while(iterator.hasNext()) {
-        sb.append(iterator.next());
-    }
-    
-    return sb.substring(0, sb.length() - k).toString();
+    this.stack.push(lastst_pushed);
   }
 }
