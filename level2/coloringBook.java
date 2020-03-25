791703e641db85e231945badeb49930edce17186
DONGWON KIM <lunacircle4@likelion.org>
2020-03-25 23:54:51 +0900

    feat: level2/coloringbook prototype

---
+++ level2/coloringBook.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+import java.util.Iterator;
+
+class ColoringBook {
+  private int[] world;
+  private int maxWideOfArea;
+  private int countOfArea;
+  
+  public ColoringBook(int[] world) {
+      this.world = world
+  }
+
+  public void service() {
+    for 배열 
+    if 라스트하고 다르면 
+        if 갯수가 최대 갯수보다 크면
+        최대갯수 갱신
+        갯수 1로 초기화
+        공간갯수 1증가
+    else
+    갯수 1증가
+    end
+  }
+
+  public static void main(String[] args) {
+    
+  }
+}
