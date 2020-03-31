aed7c5f16aab91b353e27406d4ddc7bb0d9eec64
DONGWON KIM <lunacircle4@likelion.org>
2020-03-31 23:56:00 +0900

    feat: main function prototype implement

--- level2/coloringBook.java
+++ level2/coloringBook.java
@@ -7,30 +7,41 @@ class ColoringBook {
   private int countOfArea;
   
   public ColoringBook(int[] world) {
-    this.world = world;
-    this.maxWideOfArea = 0;
-    this.countOfArea = 0;
+    if(world.length == 0)
+      this.world = new int[1];
+    else
+      this.world = Arrays.copyOf(world, world.length + 1);
+      this.world[this.world.length - 1] = this.world[1];
   }
 
   public void service() {
-    if(this.world.length == 0)
-        return;
-    
-    int lastBlock = this.world[0] + 1;
+    int countOfArea = 0;
+    int lastBlock = this.world[0];
     int wideOfArea = 0;
+    int maxWideOfArea = 0;
+
+    if(this.world.length <= 1) {
+      System.out.println(countOfArea);
+      System.out.println(maxWideOfArea);
+    }
+    
     for (int i : this.world) { 
       if(i != lastBlock) {
         if(wideOfArea > this.maxWideOfArea) 
-          this.maxWideOfArea = wideOfArea;
+          maxWideOfArea = wideOfArea;
         ++this.countOfArea; 
         wideOfArea = 1;
       }
       else
         ++wideOfArea;
     } 
+    System.out.println(countOfArea);
+    System.out.println(maxWideOfArea);
   }
 
   public static void main(String[] args) {
-    
+    int[] hoho = {1, 1, 0, 1, 0, 0};
+    ColoringBook coloringBook = new ColoringBook(hoho);
+    coloringBook.service();
   }
 }
