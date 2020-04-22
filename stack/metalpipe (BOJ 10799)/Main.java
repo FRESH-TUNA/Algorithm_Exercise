5e6ed096b325e52926fbc12ded2a14ea754ae88c
lunacircle4 <lunacircle4@likelion.org>
2020-04-23 01:33:55 +0900

    feat: BOJ 10799 implement

--- stack/metalpipe (BOJ 10799)/Main.java
+++ stack/metalpipe (BOJ 10799)/Main.java
@@ -1,15 +1,48 @@
 import java.util.LinkedList;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
-public class Main() {
-  private static void input() {
+public class Main {
+  static LinkedList<Character> stack;
+  static String line;
 
+  static void input() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    line = br.readLine();
+    br.close();
   }
 
-  private static void call() {
+  static int getNumOfPipes() {    
+    LinkedList<Character> stack 
+      = new LinkedList<Character>();;
+    int numOfPipes = 0;
+    char lastCharacter = ' ';
 
+    for(int i = 0; i < line.length(); ++i) {
+      if(line.charAt(i) == '(') 
+        stack.push('(');
+      else if(lastCharacter == ')') { 
+        stack.pop();
+        numOfPipes += 1;
+      }
+      else {
+        stack.pop();
+        if(stack.size() > 0)
+          numOfPipes += stack.size();
+      }
+      lastCharacter = line.charAt(i);
+    }
+
+    return numOfPipes;
   }
-  
-  public static void main() {
-    
+
+  static void call() {
+    System.out.println(getNumOfPipes());
+  }
+
+  public static void main(String[] args) throws IOException {
+    input();
+    call();
   }
 }
