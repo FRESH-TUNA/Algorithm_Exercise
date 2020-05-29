46cb8b8be03ab6040e1a7ca960953534db29705a
lunacircle4 <lunacircle4@likelion.org>
2020-05-29 23:53:21 +0900

    feat: boj 2294

--- dp/boj 2294/Main.java
+++ dp/boj 2294/Main.java
@@ -3,48 +3,63 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
+import java.util.Iterator;
+import java.util.TreeSet;
 
 public class Main {
   private static int coin_counts;
-  private static int coin_sum;
-  private static Treeset<Integer> coin_values;
-
-  private static void init() {
-    coin_values = new TreeSet<Integer>();
-  }
+  private static int wish;
+  private static int[] dp;
+  private static TreeSet<Integer> coins;
 
   private static void input() throws IOException {
     String[] coin_information;
     BufferedReader br 
       = new BufferedReader(new InputStreamReader(System.in));
     
-    coin_information = br.readLine().split(' ');
-    coin_counts = String.parseInt(coin_information[0]);
-    coin_sum = String.parseInt(coin_information[1]);
-
+    coins = new TreeSet<Integer>();
+    coin_information = br.readLine().split(" ");
+    coin_counts = Integer.parseInt(coin_information[0]);
+    wish = Integer.parseInt(coin_information[1]);
+    dp = new int[wish + 1];
     for(int i = 0; i < coin_counts; ++i)
-      coin_values.add(String.parseInt(br.readLine()));
+      coins.add(Integer.parseInt(br.readLine()));
     br.close();
   }
 
-  private static void call(int value) {
-    Iterator it = coin_values.iterator(); 
+  private static int getDp(int smallwish) {
+    Iterator it = coins.iterator(); 
     int min_value = Integer.MAX_VALUE;
-    // Displaying the values after iterating through the set 
-    System.out.println("The iterator values are: "); 
+
+    if(dp[smallwish] != 0)
+      return dp[smallwish];
+
+    if(smallwish == 0)
+      return 0;
+
     while (it.hasNext()) {
-      int coin = it.next();
-      if(value > coin) { 
-        int calculated = value - coin;
-        if(calculated < min_value)
-          min_value = calculated;
+      int coin = (int) it.next();
+      if(smallwish >= coin) { 
+        int temp_dp = getDp(smallwish - coin);
+        if(1 + temp_dp < min_value && temp_dp != Integer.MAX_VALUE) 
+          min_value = 1 + temp_dp;
       }
-    } 
+    }
+    dp[smallwish] = min_value;
+    return dp[smallwish];
   }
 
-  public static void main() throws IOException {
+  private static void call() {
+    for(int i = 1; i <= wish; ++i)
+      getDp(i);
+    if(dp[wish] == Integer.MAX_VALUE)
+      System.out.println(-1);
+    else
+      System.out.println(dp[wish]);
+  }
 
+  public static void main(String[] args) throws IOException {
+    input();
+    call();
   }
 }
