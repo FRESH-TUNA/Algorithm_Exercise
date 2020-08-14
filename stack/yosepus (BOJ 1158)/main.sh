fe6cd57149a6c8665829a1a576abe019c1aeee71
lunacircle4 <lunacircle4@likelion.org>
2020-08-14 15:46:10 +0900

    feat: yosepus

---
+++ stack/yosepus (BOJ 1158)/main.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+numbers=()
+head=0
+tail=0
+yosepuse_value=$1
+
+init() {
+  for ((i=1; i <= $1; i++))
+  do
+    numbers+=($i)
+  done
+}
+
+pop() {
+  value=
+}
+
+enqueue() {
+
+}
+
+nthPopAndEnqueue() {
+
+}
+
+call() {
+  init $
+}
+
+for ((i=0; i < ${#numbers[@]}; i++))
+do
+  for ((j=0; j < ${#numbers[@]}-i-1; j++))
+  do
+    if [ ${numbers[j]} -gt ${numbers[$((j+1))]} ]
+    then
+      temp=${numbers[j]}
+      numbers[$j]=${numbers[$((j+1))]}
+      numbers[$((j+1))]=$temp
+    fi
+  done
+done 
+
+echo ${numbers[@]}
